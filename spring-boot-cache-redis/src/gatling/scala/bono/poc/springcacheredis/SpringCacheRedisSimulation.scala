8eb9cfe8906eb86cd409219fcdeee1dd3d04a164
sbono <sergi.bono@lidl.es>
2021-11-18 14:39:33 +0100

    Use Redisson client

--- spring-boot-cache-redis/src/gatling/scala/bono/poc/springcacheredis/SpringCacheRedisSimulation.scala
+++ spring-boot-cache-redis/src/gatling/scala/bono/poc/springcacheredis/SpringCacheRedisSimulation.scala
@@ -1,19 +1,22 @@
 package bono.poc.springcacheredis
 
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
 
 import scala.concurrent.duration._
 
 class SpringCacheRedisSimulation extends Simulation {
-  val httpProtocol = http
+
+  val httpProtocol: HttpProtocolBuilder = http
     .baseUrl("http://localhost:8080") // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  val scn = scenario("Get from Prodducts API")
+  val scn: ScenarioBuilder = scenario("Get from Products API")
     .exec(http("Get without Cache")
       .get("/product-api-without-cache/1"))
     .exec(http("Get with Caffeine Cache")
