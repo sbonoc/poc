98c25d3b3f5cb4eacf8855fe7adf4fa6ca2ad34d
Sergi Bono <sergi.bono@gmail.com>
2025-05-09 01:44:05 +0200

    Configure Allure properly + Organize Tests

--- vue-pact-playwright/src/services/__tests__/pulseService.pact.spec.ts
+++ vue-pact-playwright/src/services/__tests__/pulseService.pact.spec.ts
@@ -1,3 +1,4 @@
+import * as allure from 'allure-js-commons'
 import { PactV4, MatchersV3, SpecificationVersion } from '@pact-foundation/pact'
 import path from 'path'
 import { expect, test } from 'vitest'
@@ -11,64 +12,83 @@ const provider = new PactV4({
   spec: SpecificationVersion.SPECIFICATION_VERSION_V4,
 })
 
-test('gets pulse when Pulse is 1', async () => {
-  const pulseExample: Pulse = { pulse: 1 }
-  const EXPECTED_BODY = MatchersV3.like(pulseExample)
-  const interaction = provider
-    .addInteraction()
-    .given('Pulse is 1')
-    .uponReceiving('a request for Pulse')
-    .withRequest('GET', '/api/pulse', (builder) => {
-      builder.query({ from: 'today' }).headers({ Accept: 'application/json' })
-    })
-    .willRespondWith(200, (builder) => {
-      builder.headers({ 'Content-Type': 'application/json' }).jsonBody(EXPECTED_BODY)
-    })
+test('Pulse API', async () => {
+  await allure.parentSuite('Integration Tests')
+  await allure.suite('Contract Tests')
+  await allure.subSuite('Pulse API')
 
-  return interaction.executeTest(async (mockserver) => {
-    if (!mockserver.url) {
-      throw new Error('Mock server URL is undefined')
-    }
+  await allure.step('gets pulse when Pulse is 1', async () => {
+    await allure.description(
+      'This test verifies that the Pulse API returns the correct pulse value when Pulse is 1',
+    )
+    await allure.severity('critical')
+    await allure.feature('Pulse API')
+    await allure.story('Get Pulse when Pulse is 1')
 
-    try {
-      const response = await getPulse(mockserver.url, 'today')
-      expect(response).toBeDefined()
-      expect(response).toBeTypeOf('object')
-      expect(Object.keys(response).length).toBeGreaterThan(0)
-      expect(response).toEqual(pulseExample)
-    } catch (error) {
-      throw new Error(`API call failed: ${error}`)
-    }
+    const pulseExample: Pulse = { pulse: 1 }
+    const EXPECTED_BODY = MatchersV3.like(pulseExample)
+    const interaction = provider
+      .addInteraction()
+      .given('Pulse is 1')
+      .uponReceiving('a request for Pulse')
+      .withRequest('GET', '/api/pulse', (builder) => {
+        builder.query({ from: 'today' }).headers({ Accept: 'application/json' })
+      })
+      .willRespondWith(200, (builder) => {
+        builder.headers({ 'Content-Type': 'application/json' }).jsonBody(EXPECTED_BODY)
+      })
+
+    return interaction.executeTest(async (mockserver) => {
+      if (!mockserver.url) {
+        throw new Error('Mock server URL is undefined')
+      }
+
+      try {
+        const response = await getPulse(mockserver.url, 'today')
+        expect(response).toBeDefined()
+        expect(response).toBeTypeOf('object')
+        expect(Object.keys(response).length).toBeGreaterThan(0)
+        expect(response).toEqual(pulseExample)
+      } catch (error) {
+        throw new Error(`API call failed: ${error}`)
+      }
+    })
   })
-})
 
-test('gets pulse when Pulse is 2', async () => {
-  const pulseExample: Pulse = { pulse: 2 }
-  const EXPECTED_BODY = MatchersV3.like(pulseExample)
-  const interaction = provider
-    .addInteraction()
-    .given('Pulse is 2')
-    .uponReceiving('a request for Pulse')
-    .withRequest('GET', '/api/pulse', (builder) => {
-      builder.query({ from: 'today' }).headers({ Accept: 'application/json' })
+  await allure.step('gets pulse when Pulse is 2', async () => {
+    await allure.description(
+      'This test verifies that the Pulse API returns the correct pulse value when Pulse is 2',
+    )
+    await allure.severity('critical')
+    await allure.feature('Pulse API')
+    await allure.story('Get Pulse when Pulse is 2')
+    const pulseExample: Pulse = { pulse: 2 }
+    const EXPECTED_BODY = MatchersV3.like(pulseExample)
+    const interaction = provider
+      .addInteraction()
+      .given('Pulse is 2')
+      .uponReceiving('a request for Pulse')
+      .withRequest('GET', '/api/pulse', (builder) => {
+        builder.query({ from: 'today' }).headers({ Accept: 'application/json' })
+      })
+      .willRespondWith(200, (builder) => {
+        builder.headers({ 'Content-Type': 'application/json' }).jsonBody(EXPECTED_BODY)
+      })
+
+    return interaction.executeTest(async (mockserver) => {
+      if (!mockserver.url) {
+        throw new Error('Mock server URL is undefined')
+      }
+
+      try {
+        const response = await getPulse(mockserver.url, 'today')
+        expect(response).toBeDefined()
+        expect(response).toBeTypeOf('object')
+        expect(Object.keys(response).length).toBeGreaterThan(0)
+        expect(response).toEqual(pulseExample)
+      } catch (error) {
+        throw new Error(`API call failed: ${error}`)
+      }
     })
-    .willRespondWith(200, (builder) => {
-      builder.headers({ 'Content-Type': 'application/json' }).jsonBody(EXPECTED_BODY)
-    })
-
-  return interaction.executeTest(async (mockserver) => {
-    if (!mockserver.url) {
-      throw new Error('Mock server URL is undefined')
-    }
-
-    try {
-      const response = await getPulse(mockserver.url, 'today')
-      expect(response).toBeDefined()
-      expect(response).toBeTypeOf('object')
-      expect(Object.keys(response).length).toBeGreaterThan(0)
-      expect(response).toEqual(pulseExample)
-    } catch (error) {
-      throw new Error(`API call failed: ${error}`)
-    }
   })
 })
--- vue-pact-playwright/vitest.config.ts
+++ vue-pact-playwright/vitest.config.ts
@@ -1,6 +1,9 @@
+import { createRequire } from "node:module";
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+
+const require = createRequire(import.meta.url);
 
 export default mergeConfig(
   viteConfig,
@@ -9,8 +12,12 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
-      setupFiles: ['allure-vitest/setup'],
-      reporters: ['default', ['allure-vitest/reporter', { resultsDir: 'allure-results' }], ['allure-playwright', { resultsDir: 'allure-results' }]]
+      setupFiles: [require.resolve('allure-vitest/setup')],
+      reporters: [
+        'default',
+        ['allure-vitest/reporter', { resultsDir: 'allure-results' }],
+        //['allure-playwright', { resultsDir: 'allure-results' }]
+      ]
     },
   }),
 )
